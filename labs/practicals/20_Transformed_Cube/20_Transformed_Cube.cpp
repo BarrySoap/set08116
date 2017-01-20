7708755c4acd24654e480f5cf7310241be39081b
Wilkie-Sullivan <40208762@live.napier.ac.uk>
2017-01-20 13:02:08 +0000

    Progress on exercise 20

--- labs/practicals/20_Transformed_Cube/20_Transformed_Cube.cpp
+++ labs/practicals/20_Transformed_Cube/20_Transformed_Cube.cpp
@@ -12,6 +12,7 @@ float theta = 0.0f;
 float rho = 0.0f;
 vec3 pos(0.0f, 0.0f, 0.0f);
 float s = 1.0f;
+float total_time = 0.0f;
 
 bool load_content() {
   // Create cube data - twelve triangles triangles
@@ -20,23 +21,53 @@ bool load_content() {
       // *********************************
       // Add the position data for triangles here, (6 verts per side)
       // Front
+	  vec3(-1.0f, 1.0f, 0.0f),
+	  vec3(-1.0f, -1.0f, 0.0f),
+	  vec3(1.0f, -1.0f, 0.0f),
 
-
+	  vec3(-1.0f, 1.0f, 0.0f),
+	  vec3(1.0f, -1.0f, 0.0f),
+	  vec3(1.0f, 1.0f, 0.0f),
       // Back
+	  vec3(1.0f, -1.0f, -2.0f),
+	  vec3(-1.0f, -1.0f, -2.0f),
+	  vec3(-1.0f, 1.0f, -2.0f),
 
-
+	  vec3(1.0f, 1.0f, -2.0f),
+	  vec3(1.0f, -1.0f, -2.0f),
+	  vec3(-1.0f, 1.0f, -2.0f),
       // Right
+	  vec3(1.0f, -1.0f, 0.0f),
+	  vec3(1.0f, -1.0f, -2.0f),
+	  vec3(1.0f, 1.0f, 0.0f),
 
-
+	  vec3(1.0f, 1.0f, 0.0f),
+	  vec3(1.0f, -1.0f, -2.0f),
+	  vec3(1.0f, 1.0f, -2.0f),
       // Left
+	  vec3(-1.0f, 1.0f, 0.0f),
+	  vec3(-1.0f, 1.0f, -2.0f),
+	  vec3(-1.0f, -1.0f, -2.0f),
 
-
+	  vec3(-1.0f, 1.0f, 0.0f),
+	  vec3(-1.0f, -1.0f, -2.0f),
+	  vec3(-1.0f, -1.0f, 0.0f),
       // Top
+	  vec3(1.0f, 1.0f, -2.0f),
+	  vec3(-1.0f, 1.0f, 0.0f),
+	  vec3(1.0f, 1.0f, 0.0f),
 
-
+	  vec3(1.0f, 1.0f, -2.0f),
+	  vec3(-1.0f, 1.0f, -2.0f),
+	  vec3(-1.0f, 1.0f, 0.0f),
       // Bottom
+	  vec3(1.0f, -1.0f, 0.0f),
+	  vec3(-1.0f, -1.0f, 0.0f),
+	  vec3(1.0f, -1.0f, -2.0f),
 
-
+	  vec3(-1.0f, -1.0f, 0.0f),
+	  vec3(-1.0f, -1.0f, -2.0f),
+	  vec3(1.0f, -1.0f, -2.0f),
       // *********************************
   };
   // Colours
@@ -64,12 +95,33 @@ bool load_content() {
 
 bool update(float delta_time) {
   // *********************************
+  // Accumulate time
   // Use keys to update transform values
   // WSAD - movement
   // Cursor - rotation
   // O decrease scale, P increase scale
 
+	/** Checking Time **/
+	total_time += delta_time;
+	s = 1.0f + sinf(total_time);
+	s *= 5.0f;
+	theta += pi<float>() * delta_time;
+	/*******************/
 
+	/** WASD Movement **/
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_UP)) {
+		pos += vec3(0.0f, 0.0f, -5.0f) * delta_time;
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_DOWN)) {
+		pos += vec3(0.0f, 0.0f, 5.0f) * delta_time;
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_LEFT)) {
+		pos += vec3(-5.0f, 0.0f, 0.0f) * delta_time;
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_RIGHT)) {
+		pos += vec3(5.0f, 0.0f, 0.0f) * delta_time;
+	}
+	/*******************/
 
 
 
@@ -110,10 +162,10 @@ bool render() {
   mat4 T, R, S, M;
   // *********************************
   // Create transformation matrix
-
-
-
-
+  S = scale(mat4(s), vec3(s, s, s));
+  R = rotate(mat4(1.0f), theta, vec3(0.0f, 1.0f, 0.0f));
+  T = translate(mat4(1.0f), pos);
+  M = T * (R * S);
   // *********************************
   // Create MVP matrix
   auto V = cam.get_view();
