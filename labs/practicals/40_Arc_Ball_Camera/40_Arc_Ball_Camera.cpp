f46e34fdf0043b6a7551cb7ef057c03a123af8f3
Glenn Sullivan <Glenn Sullivan>
2017-02-14 18:59:44 +0000

    Finished exercise 40?

--- labs/practicals/40_Arc_Ball_Camera/40_Arc_Ball_Camera.cpp
+++ labs/practicals/40_Arc_Ball_Camera/40_Arc_Ball_Camera.cpp
@@ -16,9 +16,9 @@ double cursor_y = 0.0;
 bool initialise() {
   // *********************************
   // Set input mode - hide the cursor
-
+	glfwSetInputMode(renderer::get_window(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
   // Capture initial mouse position
-
+	glfwGetCursorPos(renderer::get_window(), &cursor_x, &cursor_y);
   // *********************************
 
   return true;
@@ -88,51 +88,46 @@ bool update(float delta_time) {
   double current_y;
   // *********************************
   // Get the current cursor position
-
+  glfwGetCursorPos(renderer::get_window(), &current_x, &current_y);
   // Calculate delta of cursor positions from last frame
-
-
+  double delta_x = current_x - cursor_x;
+  double delta_y = current_y - cursor_y;
   // Multiply deltas by ratios and delta_time - gets actual change in orientation
-
-
+  delta_x = delta_x * ratio_width;
+  delta_y = delta_y * ratio_height;
   // Rotate cameras by delta
   // delta_y - x-axis rotation
   // delta_x - y-axis rotation
-
+  cam.rotate(delta_y, delta_x);
   // Use keyboard to move the target_mesh- WSAD
   // Also remember to translate camera
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  if (glfwGetKey(renderer::get_window(), GLFW_KEY_W)) {
+	  target_mesh.get_transform().position += vec3(0.0f, 0.0f, -1.0f);
+	  cam.translate(vec3(0.0f, 0.0f, -1.0f));
+  }
+  if (glfwGetKey(renderer::get_window(), GLFW_KEY_A)) {
+	  target_mesh.get_transform().position += vec3(-1.0f, 0.0f, 0.0f);
+	  cam.translate(vec3(-1.0f, 0.0f, 0.0f));
+  }
+  if (glfwGetKey(renderer::get_window(), GLFW_KEY_S)) {
+	  target_mesh.get_transform().position += vec3(0.0f, 0.0f, 1.0f);
+	  cam.translate(vec3(0.0f, 0.0f, 1.0f));
+  }
+  if (glfwGetKey(renderer::get_window(), GLFW_KEY_D)) {
+	  target_mesh.get_transform().position += vec3(1.0f, 0.0f, 0.0f);
+	  cam.translate(vec3(1.0f, 0.0f, 0.0f));
+  }
   // Use UP and DOWN to change camera distance
-
-
-
-
-
-
+  if (glfwGetKey(renderer::get_window(), GLFW_KEY_UP)) {
+	  cam.move(1);
+  }
+  if (glfwGetKey(renderer::get_window(), GLFW_KEY_DOWN)) {
+	  cam.move(-1);
+  }
   // Update the camera
-
+  cam.update(delta_time);
   // Update cursor pos
-
-
+  glfwSetCursorPos(renderer::get_window(), cursor_x, cursor_y);
   // *********************************
   return true;
 }
