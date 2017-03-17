7cb0108de425002ebc9bc39389c8d87d3f10ce1e
Wilkie-Sullivan <40208762@live.napier.ac.uk>
2017-03-17 10:43:11 +0000

    Finished exercise 61

--- labs/practicals/61_Fog/61_Fog.cpp
+++ labs/practicals/61_Fog/61_Fog.cpp
@@ -15,6 +15,7 @@ effect eff;
 texture tex;
 target_camera cam;
 directional_light light;
+int fog = 0;
 
 bool load_content() {
   // Create plane mesh
@@ -122,6 +123,15 @@ bool update(float delta_time) {
   if (glfwGetKey(renderer::get_window(), '4')) {
     cam.set_position(vec3(50, 10, -50));
   }
+  if (glfwGetKey(renderer::get_window(), '5')) {
+	  fog = 0;
+  }
+  if (glfwGetKey(renderer::get_window(), '6')) {
+	  fog = 1;
+  }
+  if (glfwGetKey(renderer::get_window(), '7')) {
+	  fog = 2;
+  }
 
   // Rotate the sphere
   meshes["sphere"].get_transform().rotate(vec3(0.0f, half_pi<float>(), 0.0f) * delta_time);
@@ -173,7 +183,7 @@ bool render() {
 	// Set fog density: 0.04f
 	glUniform1f(eff.get_uniform_location("fog_density"), 0.04f);
 	// Set fog type: FOG_EXP2
-	glUniform1i(eff.get_uniform_location("fog_type"), FOG_EXP2);
+	glUniform1i(eff.get_uniform_location("fog_type"), fog);
     // *********************************
 	
     // Render mesh
